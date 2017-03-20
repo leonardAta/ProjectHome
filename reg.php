7d03cf63a8910abae19b23e42a572a1cd354b4b0
Leonard Ata <atatangban@live.com>
2017-03-21 00:57:31 +0100

    creating the registration page

---
+++ reg.php
@@ -0,0 +1,16 @@
+<?php 
+
+?>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<h1>Registration Portal</h1>
+	<form action="" method="post">
+		<p>Firstname: <input type="text" name="firstname"/></p>
+		<p>Lastname : <input type="text" name="lastname"/></p>
+
+	</form>
+</body>
+</html>
