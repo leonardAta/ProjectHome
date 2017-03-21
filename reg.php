21afe7c496eee5d43e2fe4c2fd4541e551a27637
Leonard Ata <atatangban@live.com>
2017-03-21 10:06:03 +0100

    Password added

--- reg.php
+++ reg.php
@@ -10,6 +10,9 @@
 	<form action="" method="post">
 		<p>Firstname: <input type="text" name="firstname"/></p>
 		<p>Lastname : <input type="text" name="lastname"/></p>
+		<p>Gender: Male<input type="radio" name="gender" value="male"/>
+				   Female <input type="radio" name="gender" value="female"/>
+		<p>Password: <input type="password" name="password"/>
 
 	</form>
 </body>
